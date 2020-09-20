1e86e547d9bdb56ef243063ad78d99c6b5c42fa0
sarimarton <sarimarton@gmail.com>
2020-09-21 01:43:40 +0200

    Update

--- vps-init.sh
+++ vps-init.sh
@@ -4,27 +4,25 @@
 # Run it from inside the VPS with
 # curl -fsS https://raw.githubusercontent.com/sarimarton/vps-init/master/vps-init.sh | bash
 
-# Or from outside:
-# ssh root@server.com 'set -s pipefail ; curl -fsS https://raw.githubusercontent.com/sarimarton/vps-init/master/vps-init.sh | bash'
+# This script assumes that you're in an SSH session of a fresh CentOS 8 install
+# with the root user.
 
-# Setup up users
-echo Setting up users...
-
-# Set password with checking if there's a preset value
+# Setup users (check if there's pre-defined passwords)
 [ -z "$ROOTPW" ] && passwd root || echo -e "$ROOTPW\n$ROOTPW" | passwd root
 adduser marci
-
-# Set password with checking if there's a preset value
 [ -z "$MARCIPW" ] && passwd marci || echo -e "$MARCIPW\n$MARCIPW" | passwd marci
 usermod -aG wheel marci
 
-# Install utilities
-echo Installing utilities...
+# Install stuff
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+# dnf install -y gcc-c++ make
+dnf install -y mc nginx git nodejs
 
-dnf install -y mc
+# Build up marci stuff
+runuser -l  marci -c 'mkdir ~/projects'
+runuser -l  marci -c 'mkdir ~/static'
+runuser -l  marci -c 'mkdir ~/tmp'
 
-# Install nginx
-echo Installing nginx...
-
-dnf install -y nginx
-
+# Final touch
+systemctl enable nginx
+systemctl start nginx
