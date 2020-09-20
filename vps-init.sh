e35aac6a52d1edc3a75ddc60fcb4137df04e5406
sarimarton <sarimarton@gmail.com>
2020-09-21 00:56:11 +0200

    Test

--- vps-init.sh
+++ vps-init.sh
@@ -13,6 +13,7 @@ echo Setting up users...
 # Set password with checking if there's a preset value
 [ -z "$ROOTPW" ] && passwd root || echo -e "$ROOTPW\n$ROOTPW" | passwd root
 adduser marci
+echo $MARCIPW
 # Set password with checking if there's a preset value
 [ -z "$MARCIPW" ] && passwd marci || echo -e "$MARCIPW\n$MARCIPW" | passwd marci
 usermod -aG wheel marci
