e92d098312af5eb4e5bd0b74e7d0b9cb443c8f54
sarimarton <sarimarton@gmail.com>
2020-09-21 00:26:53 +0200

    Update

--- vps-init.sh
+++ vps-init.sh
@@ -1,5 +1,29 @@
-
-# VPS init script
+# VPS install script
 # For CentOS 8
 
-echo hello
+# Run it from inside the VPS with
+# curl -fsS https://raw.githubusercontent.com/sarimarton/vps-init/master/vps-init.sh | bash
+
+# Or from outside:
+# ssh root@server.com 'set -s pipefail ; curl -fsS https://raw.githubusercontent.com/sarimarton/vps-init/master/vps-init.sh | bash'
+
+# Setup up users
+echo Setting up users...
+
+# Set password with checking if there's a preset value
+[ -z "$ROOTPW" ] && passwd root || echo -e "$ROOTPW\n$ROOTPW" | passwd root
+adduser marci
+# Set password with checking if there's a preset value
+[ -z "$MARCIPW" ] && passwd root || echo -e "$MARCIPW\n$MARCIPW" | passwd root
+usermod -aG wheel marci
+
+# Install utilities
+echo Installing utilities...
+
+dnf install -y mc
+
+# Install nginx
+echo Installing nginx...
+
+dnf install -y nginx
+
