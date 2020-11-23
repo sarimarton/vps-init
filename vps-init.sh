7982209adf97e351d24398f796a6ed83a9aab160
sarimarton <sarimarton@gmail.com>
2020-11-23 03:29:58 +0100

    Fix permission

--- vps-init.sh
+++ vps-init.sh
@@ -20,7 +20,8 @@ usermod -aG wheel marci
 curl -sS https://get.docker.com/ | sh
 systemctl enable docker
 systemctl start docker
-sudo usermod -aG docker marci
+usermod -aG docker marci
+chmod 755 /home/marci
 curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
